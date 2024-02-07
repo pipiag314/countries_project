8709f7957a2b630584f2d36175c6c0bb2d8b4d02
gela pipia <gelapipia10@gmail.com>
2024-02-07 14:33:01 +0400

    ADD: types for airports

---
+++ src/types/airportsType.ts
@@ -0,0 +1,11 @@
+type AirportsType = AirportInterface[];
+
+interface AirportInterface {
+    country: string;
+    elevation_ft: string;
+    icao: string;
+    city: string;
+    name: string;
+    region: string;
+    timezone: string;
+}
